ae01663b6688aa8ab937df642aab32b30567839a
plachta11b <janholcak@gmail.com>
2016-09-19 19:39:38 +0200

    add message for missing config

--- src/config.py
+++ src/config.py
@@ -12,7 +12,16 @@ class Config:
 		script_directory = path.dirname(path.realpath(__file__))
 
 		self.path_to_config = path_to_config
-		self.file_config = yaml.load(open(path.join(script_directory, "./../config/config.yml"), 'r'))
+
+		try:
+			config_file_descriptor = open(path.join(script_directory, path_to_config), 'r')
+		except IOError as error:
+			if error.args[0] == 2:
+				print("Did you have created config from config example?")
+				
+			exit("ERROR Can not open config file: " + error.args[1])
+
+		self.file_config = yaml.load(config_file_descriptor)
 
 	def get_source_connection_configuration(self):
 		return self.file_config["connection_source"]
